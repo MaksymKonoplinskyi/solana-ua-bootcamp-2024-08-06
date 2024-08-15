ba4913e4a2e17e8f4cc7c3a731b726f8aa90a4b2
MaksymKonoplinskyi <gran7777777@gmail.com>
2024-08-15 10:45:06 +0300

    feat: first lesson with star

---
+++ practice-1/key-grinder.ts
@@ -0,0 +1,22 @@
+import { Keypair } from '@solana/web3.js'
+import { fromByteArray } from 'base64-js'
+
+const startTime = Date.now()
+
+let keypair
+let publicKeyBase58 = ''
+
+do {
+  keypair = Keypair.generate()
+  publicKeyBase58 = keypair.publicKey.toBase58()
+} while (!publicKeyBase58.startsWith('LAVA')) // Префікс проекту, розробкою якого я зараз займаюсь (LAVARAGE)
+
+const secretKeyBase64 = fromByteArray(keypair.secretKey)
+
+const endTime = Date.now()
+const timeTaken = (endTime - startTime) / 1000 // В секундах
+
+console.log(`The public key is: `, publicKeyBase58)
+console.log(`The secret key is: `, secretKeyBase64)
+console.log(`Time taken: ${timeTaken} seconds`)
+console.log(`✅ Finished!`)
